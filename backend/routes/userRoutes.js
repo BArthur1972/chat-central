97b0865ba66db6aafc41eb69cf81cd8614038ce4
Bethel Arthur <kbarthur14@gmail.com>
2024-02-26 18:13:08 -0600

    Added route handlers for some actions on the Account Page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-// Creating a new user
+// Get user by id
+router.get('/getUserById/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.log("Error: ", error.message);
+    }
+});
+
+// Creating a new user or signing up
 router.post('/', async (req, res) => {
     try {
         const { name, email, password, picture, bio } = req.body;
-        console.log(req.body);
         const user = await User.create({ name, email, password, picture, dateJoined: Date.now() , bio});
         res.status(201).json(user);
     } catch (e) {
@@ -30,6 +41,47 @@ router.post('/login', async (req, res) => {
         res.status(200).json(user);
     } catch (e) {
         res.status(400).json(e.message);
+    }
+});
+
+// Update the username
+router.put('/updateUsername/:id/:newName', async (req, res) => {
+    const { id, newName } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(id, { name: newName }, { new: true });
+        res.status(200).json(user);
+        console.log("User updated: ", user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.log("Error: ", error.message);
+    }
+});
+
+// Update password
+router.put('/updatePassword/:id/:newPassword', async (req, res) => {
+    const { id, newPassword } = req.params;
+    try {
+        const user = await User.findById(id);
+        user.password = newPassword;
+        await user.save();
+        res.status(200).json(user);
+        console.log("User updated: ", user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.log("Error: ", error.message);
+    }
+});
+
+// Update bio
+router.put('/updateBio/:id/:newBio', async (req, res) => {
+    const { id, newBio } = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(id, { bio: newBio }, { new: true });
+        res.status(200).json(user);
+        console.log("User updated: ", user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.log("Error: ", error.message);
     }
 });
 
